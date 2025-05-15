ee8a8b1740b4893908da3ea73cfeab3568623b08
sofiawolf2 <ouniversodesofiawolf@gmail.com>
2025-05-15 12:47:34 -0300

    primeiro commit

---
+++ src/main/java/br/com/taurus/gestor/GestorPmeApplication.java
@@ -0,0 +1,13 @@
+package br.com.taurus.gestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorPmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorPmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/taurus/gestor/GestorPmeApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.taurus.gestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorPmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert (true);
+	}
+
+}
