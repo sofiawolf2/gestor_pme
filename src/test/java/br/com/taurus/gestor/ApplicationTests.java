07bfaeef8bdc0d4b64c33035a22920233d722891
sofiawolf2 <ouniversodesofiawolf@gmail.com>
2025-05-15 13:58:32 -0300

    renomear arquivo

--- src/test/java/br/com/taurus/gestor/ApplicationTests.java
+++ src/test/java/br/com/taurus/gestor/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestorPmeApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
