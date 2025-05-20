23c165bf68818a919b51cf83a8a725705f051d2a
sofiawolf2 <ouniversodesofiawolf@gmail.com>
2025-05-20 13:03:27 -0300

    erros corrigidos

--- init.sql
+++ init.sql
@@ -5,21 +5,21 @@ CREATE TABLE IF NOT EXISTS status (
     descricao VARCHAR(100) NOT NULL
 );
 
-INSERT INTO status (nome, descricao) VALUES
+INSERT INTO status (id, descricao) VALUES
 (1,'ABERTA'),
 (2,'VENCIDA'),
 (3,'PAGA'),
 (4,'CANCELADA'),
 (5,'PARCELADA'),
 (6,'ESTORNADA'),
-(7,'EM_DISPUTA);
+(7,'EM_DISPUTA');
 
 CREATE TABLE IF NOT EXISTS categoria(
     id INTEGER PRIMARY KEY,
     descricao VARCHAR(100) NOT NULL
 );
 
-INSERT INTO categoria (nome, descricao) VALUES
+INSERT INTO categoria (id, descricao) VALUES
 (1,'DESPESA_FIXA'),
 (2,'DESPESA_VARIAVEL'),
 (3,'RECEITA_RECORRENTE'),
@@ -32,43 +32,51 @@ CREATE TABLE IF NOT EXISTS origem (
     descricao VARCHAR(100) NOT NULL
 );
 
-INSERT INTO origem (nome, descricao) VALUES
+INSERT INTO origem (id, descricao) VALUES
 (1,'FORNECEDOR'),
 (2,'CLIENTE'),
 (3,'FUNCIONARIO'),
 (4,'INSTITUICAO_FINANCEIRA'),
 (5,'GOVERNO');
 
+CREATE TABLE IF NOT EXISTS role (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(20)
+);
+INSERT INTO role (id, nome) VALUES (1,'ROLE_ADMIN'), (2,'ROLE_USER');
+
 CREATE TABLE IF NOT EXISTS usuario (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    nome VARCHAR(100),
-    login VARCHAR(50),
-    senha VARCHAR(255),
-    email VARCHAR(50)
+        nome VARCHAR(100),
+        login VARCHAR(50),
+        senha VARCHAR(255),
+        email VARCHAR(50),
+        role_id SERIAL,
+        FOREIGN KEY (role_id) REFERENCES "role" (id)
 );
 
-INSERT INTO usuario (id, nome, login, senha, email) VALUES
-('e9b1f85d-4a58-4c2e-bb8b-3a41f8a9d1c7', 'Sofia Dev', 'dev', '123456','dev@gmail.com'),
-('f3a6df49-8b26-4890-9b56-2ddc94e8f1f1', 'User Teste', 'teste', '123456','teste@gmail.com');
+INSERT INTO usuario (id, nome, login, senha, email, role_id) VALUES
+('e9b1f85d-4a58-4c2e-bb8b-3a41f8a9d1c7', 'Sofia Dev', 'dev', '123456','dev@gmail.com', 1),
+('f3a6df49-8b26-4890-9b56-2ddc94e8f1f1', 'User Teste', 'teste', '123456','teste@gmail.com', 2);
 
 CREATE TABLE IF NOT EXISTS conta (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     vencimento DATE NOT NULL,
     descricao VARCHAR (100) NOT NULL,
     valor DOUBLE PRECISION NOT NULL,
     data_pagamento DATE,
     observacao TEXT,
     imagem TEXT,
-    status_enum VARCHAR(30),
-    origem_enum VARCHAR(30),
-    categoria_enum VARCHAR(30),
+    status_id INTEGER,
+    origem_id INTEGER,
+    categoria_id INTEGER,
     usuario_id UUID,
-    FOREIGN KEY (status_enum) REFERENCES "status" (nome),
-    FOREIGN KEY (origem_enum) REFERENCES "origem" (nome),
-    FOREIGN KEY (categoria_enum) REFERENCES "categoria" (nome),
+    FOREIGN KEY (status_id) REFERENCES "status" (id),
+    FOREIGN KEY (origem_id) REFERENCES "origem" (id),
+    FOREIGN KEY (categoria_id) REFERENCES "categoria" (id),
     FOREIGN KEY (usuario_id) REFERENCES "usuario" (id)
 );
 
-INSERT INTO conta (id, vencimento, descricao, valor, data_pagamento, observacao, imagem, status_enum, origem_enum, categoria_enum, usuario_id)
+INSERT INTO conta (id, vencimento, descricao, valor, data_pagamento, observacao, imagem, status_id, origem_id, categoria_id, usuario_id)
 VALUES
-(1, '2026-01-01', 'Conta teste', 2000.00, null, 'é isso', 'sem imagem', 'ABERTA', 'CLIENTE', 'DESPESA_FIXA', 'f3a6df49-8b26-4890-9b56-2ddc94e8f1f1');
+(1, '2026-01-01', 'Conta teste', 2000.00, null, 'é isso', 'sem imagem', 1, 1, 1, 'f3a6df49-8b26-4890-9b56-2ddc94e8f1f1');
