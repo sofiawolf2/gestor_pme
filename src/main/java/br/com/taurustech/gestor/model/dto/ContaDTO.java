312604f8e88de636e88b464047ecb9e2ea3e0dcf
sofiawolf2 <ouniversodesofiawolf@gmail.com>
2025-05-22 15:51:07 -0300

    ajustes

--- src/main/java/br/com/taurustech/gestor/model/dto/ContaDTO.java
+++ src/main/java/br/com/taurustech/gestor/model/dto/ContaDTO.java
@@ -11,7 +11,7 @@ public class ContaDTO {
     private Integer id;
     private String vencimento;
     private String descricao;
-    private Double valor;
+    private String valor;
     private String dataPagamento;
     private String observacao;
     private String imagem;
@@ -20,25 +20,27 @@ public class ContaDTO {
     private String categoria;
     private String user;
 
-    private static ContaDTO createOutput(Conta conta){
+    public static ContaDTO createOutput(Conta conta){
         ModelMapper modelMapper = new ModelMapper();
         var dto = modelMapper.map(conta, ContaDTO.class);
         dto.vencimento = conta.getVencimento().toString();
-        dto.dataPagamento = conta.getDataPagamento().toString();
+        dto.valor = conta.getValor().toString();
+        if (conta.getDataPagamento()!=null) dto.dataPagamento = conta.getDataPagamento().toString();
         dto.status = conta.getStatus().getDescricao();
         dto.origem = conta.getOrigem().getDescricao();
         dto.categoria = conta.getCategoria().getDescricao();
-        dto.user = conta.getUser().getId().toString();
+        dto.user = null;
         return dto;
     }
 
     public static ContaDTO createInput(Conta conta){
         var dto = createOutput(conta);
         dto.id = null;
+        dto.user = conta.getUser().getId().toString();
         return dto;
     }
 
-    public Conta getContaSemEntidades (){
+    public Conta gerarContaSemEntidades () {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper.map(this, Conta.class);
     }
