c5a4fcc834133720000fa0e395902c089779c287
sofiawolf2 <ouniversodesofiawolf@gmail.com>
2025-05-22 15:50:48 -0300

    reajuste de local, add listar todos

--- src/main/java/br/com/taurustech/gestor/service/ContaService.java
+++ src/main/java/br/com/taurustech/gestor/service/ContaService.java
@@ -1,33 +1,66 @@
 package br.com.taurustech.gestor.service;
 
+import br.com.taurustech.gestor.exception.ObjetoNaoEncontradoException;
+import br.com.taurustech.gestor.model.Categoria;
 import br.com.taurustech.gestor.model.Conta;
+import br.com.taurustech.gestor.model.Origem;
+import br.com.taurustech.gestor.model.Status;
 import br.com.taurustech.gestor.model.dto.ContaDTO;
+import br.com.taurustech.gestor.repository.CategoriaRepository;
 import br.com.taurustech.gestor.repository.ContaRepository;
+import br.com.taurustech.gestor.repository.OrigemRepository;
+import br.com.taurustech.gestor.repository.StatusRepository;
 import br.com.taurustech.gestor.validator.ContaValidator;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.List;
+
+@Service @RequiredArgsConstructor
 public class ContaService {
     private final ContaRepository contaRepository;
     private final ContaValidator contaValidator;
+    private final StatusRepository statusRepository;
+    private final OrigemRepository origemRepository;
+    private final CategoriaRepository categoriaRepository;
+    private final UserService userService;
 
-    public ContaService(ContaRepository contaRepository, ContaValidator contaValidator) {
-        this.contaRepository = contaRepository;
-        this.contaValidator = contaValidator;
+
+    String erroNotFound = "Conta não encontrada";
+
+    private Conta gerarEntidade (ContaDTO dto){
+        Conta conta = dto.gerarContaSemEntidades();
+
+        if (userService.buscarUserAtual().getRole().getNome().equals("ROLE_ADMIN")&& dto.getUser() != null){
+            conta.setUser(userService.buscarUserID(dto.getUser()));
+        }else {conta.setUser(userService.buscarUserAtual());}
+
+        conta.setStatus(statusRepository.findByDescricaoIgnoreCase(dto.getStatus()));
+        conta.setCategoria(categoriaRepository.findByDescricaoIgnoreCase(dto.getCategoria()));
+        conta.setOrigem(origemRepository.findByDescricaoIgnoreCase(dto.getOrigem()));
+
+        return conta;
     }
 
-    String erroNotFound = "Conta não encontrado";
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
-
     public void cadastrar(ContaDTO contaDTO) {
-        Conta conta = contaDTO.getContaSemEntidades();
-        Conta contaComEntidades = contaValidator.validarERetornarEntidades(contaDTO);
-        conta.setStatus(contaComEntidades.getStatus());
-        conta.setCategoria(contaComEntidades.getCategoria());
-        conta.setOrigem(contaComEntidades.getOrigem());
-        conta.setUser(contaComEntidades.getUser());
+        contaValidator.validarDatasConta(contaDTO);
+        contaRepository.save(gerarEntidade(contaDTO));
+    }
 
-        contaRepository.save(conta);
+    public List<ContaDTO> listarContas(String status, String origem, String categoria) {
+        var conta = new Conta();
+        conta.setStatus(new Status(status));
+        conta.setOrigem(new Origem(origem));
+        conta.setCategoria(new Categoria(categoria));
+        conta.setUser(userService.buscarUserAtual());
+
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreNullValues().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Conta> contaExample = Example.of(conta,matcher);
+
+        var lista = contaRepository.findAll(contaExample);
+        if (lista.isEmpty()) throw new ObjetoNaoEncontradoException(erroNotFound);
+        return lista.stream().map(ContaDTO::createOutput).toList();
     }
 }
--- src/main/java/br/com/taurustech/gestor/validator/ContaValidator.java
+++ src/main/java/br/com/taurustech/gestor/validator/ContaValidator.java
@@ -1,33 +1,23 @@
 package br.com.taurustech.gestor.validator;
 
-import br.com.taurustech.gestor.model.Conta;
 import br.com.taurustech.gestor.model.dto.ContaDTO;
-import br.com.taurustech.gestor.repository.CategoriaRepository;
-import br.com.taurustech.gestor.repository.OrigemRepository;
-import br.com.taurustech.gestor.repository.StatusRepository;
-import br.com.taurustech.gestor.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import static br.com.taurustech.gestor.validator.ValidatorUtil.isDate;
+import static br.com.taurustech.gestor.validator.ValidatorUtil.isDouble;
 
 @Component
 @RequiredArgsConstructor
 public class ContaValidator {
-    private final StatusRepository statusRepository;
-    private final OrigemRepository origemRepository;
-    private final CategoriaRepository categoriaRepository;
-    private final UserService userService;
-    String textoInvalido = "valor invalido";
 
-    public Conta validarERetornarEntidades(ContaDTO dto){
+
+    public void validarDatasConta(ContaDTO dto){
         isDate(dto.getVencimento(), "vencimento");
         isDate(dto.getDataPagamento(), "dataPagamento");
-        Conta conta = new Conta();
-        conta.setUser(userService.buscarUserID(dto.getUser()));
-        conta.setCategoria(categoriaRepository.findByDescricaoIgnoreCase(dto.getCategoria()));
-        conta.setOrigem(origemRepository.findByDescricaoIgnoreCase(dto.getOrigem()));
-        conta.setStatus(statusRepository.findByDescricaoIgnoreCase(dto.getStatus()));
-        return conta;
+        isDouble(dto.getValor(), "valor");
+
     }
+
+
 }
