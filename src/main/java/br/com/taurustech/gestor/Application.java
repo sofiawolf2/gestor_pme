90c35195e398ee585479955f4c66b0af63438e5f
sofiawolf2 <ouniversodesofiawolf@gmail.com>
2025-06-02 16:02:15 -0300

    add EnableJpaAuditing

--- src/main/java/br/com/taurustech/gestor/Application.java
+++ src/main/java/br/com/taurustech/gestor/Application.java
@@ -2,8 +2,9 @@ package br.com.taurustech.gestor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication @EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
